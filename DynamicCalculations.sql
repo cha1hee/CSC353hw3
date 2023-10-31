f31dfb9e258e013274d9900e842ed87a53a4df10
Kate Hynes <kahynes@davidson.edu>
2023-10-30 20:53:39 -0400

    added showAggregateStatistics

--- DynamicCalculations.sql
+++ DynamicCalculations.sql
@@ -1,3 +1,4 @@
+-- EXAMPLE FUNCTION
 DROP FUNCTION IF EXISTS dept_countF;
 DELIMITER //
 CREATE FUNCTION dept_countF(dept_name VARCHAR(20)) RETURNS INTEGER READS SQL DATA
@@ -18,6 +19,8 @@ DELIMITER ;
 SELECT dept_countF(‘History’); -- should output 2
 SET @historyCount = dept_countF(‘History’);
 SELECT @historyCount; -- should also output 2
+
+-- EXAMPLE PROCEDURE
 DROP PROCEDURE IF EXISTS dept_countP;
 DELIMITER //
 CREATE PROCEDURE dept_countP(IN dept_name VARCHAR(20), OUT dcount INT)
@@ -30,3 +33,43 @@ END
 DELIMITER ;
 CALL dept_countP(‘History’, @xyz);
 SELECT @xyz; -- should output 2
+
+
+
+-- QUESTION # 2: showAggregateStatistics
+DROP PROCEDURE IF EXISTS showAggregateStatistics;
+DELIMITER //
+CREATE PROCEDURE showAggregateStatistics(IN name VARCHAR(33), start DATE, finish DATE)
+BEGIN
+SELECT 
+	player.name AS name, 
+	AVG(ace) AS aces, 
+	AVG(df) AS double_faults, 
+	AVG(fstIn) AS first_in 
+		FROM plays, player, tournament, matchinfo 
+			WHERE 
+				player.id = plays.player_id AND 
+				player.name = name AND 
+				tournament.tourn_date > start AND 
+				tournament.tourn_date < finish AND 
+				matchinfo.tourney_id = tournament.id AND 
+				plays.match_id = matchinfo.match_id
+		GROUP BY player.name;
+END
+//
+DELIMITER ;
+-- tested with:
+CALL showAggregateStatistics('Taylor Fritz', '1968-11-21', '2023-01-02');
+
+-- FOR TESTING PURPOSES
+-- test procedure (just displays all data for the person within selected date range)
+DROP PROCEDURE IF EXISTS testAggregateStatistics;
+DELIMITER //
+CREATE PROCEDURE testAggregateStatistics(IN name VARCHAR(33), start DATE, finish DATE)
+BEGIN
+SELECT player.name AS name, ace, df, fstIn FROM plays, player, tournament, matchinfo 
+WHERE player.id = plays.player_id AND player.name = name AND tournament.tourn_date > start AND tournament.tourn_date < finish AND matchinfo.tourney_id = tournament.id AND plays.match_id = matchinfo.match_id;
+END
+//
+DELIMITER ;
+CALL testAggregateStatistics('Taylor Fritz', '1968-11-21', '2023-01-02');
